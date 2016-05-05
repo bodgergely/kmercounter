870104e92ad65e47070e593ae57b4f245ef5bcdf
Gergely Bod <gergely.bod@morganstanley.com>
2016-05-05 15:27:33 +0100

    remove printf

--- src/count.cpp
+++ src/count.cpp
@@ -18,11 +18,21 @@ using namespace std;
 int main(int argc, char** argv)
 {
 	const char* str = "abcbdebbfdfdfebb";
+	int k = 3;
+	int n = 10;
+
+	if(argc > 1)
+	{
+		str = argv[1];
+		k = atoi(argv[2]);
+		n = atoi(argv[3]);
+	}
+
+
 	size_t len = strlen(str);
 	char* input = (char*)malloc((len+1) * sizeof(char));
 	strcpy(input, str);
-	printf("%s\n", input);
-	KmerProcessor processor(input,len,3, 15, 1);
+	KmerProcessor processor(input,len,k, n, 1);
 
 	vector<pair<string, size_t>> results;
 	processor.process(results);
