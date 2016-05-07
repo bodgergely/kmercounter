145165917009016960b0f777113c88ccfed062b7
Gergely Bod <bodgergely@hotmail.com>
2016-05-07 01:01:03 +0100

    genome file generator python script added

---
+++ scripts/genome_gen.py
@@ -0,0 +1,9 @@
+import random
+
+f = open("../inputs/medium", "w")
+chars = ('a', 'c', 'g', 't', 'n')
+for i in xrange(160000):
+    c = random.randint(0,len(chars)-1)
+    f.write(chars[c])
+    
+f.close()
